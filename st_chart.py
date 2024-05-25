d0f2e8000c61d96fe08757a72b6bd6026ef3dc6c
Hiroji Fukui <hiroji@gmail.com>
2024-05-25 11:25:45 -0700

    Removed comments

--- st_chart.py
+++ st_chart.py
@@ -9,22 +9,21 @@ st.sidebar.title("Test Results")
 st.sidebar.dataframe(df) 
 
 #Bar Graph
-# st.title("Math")
-# st.bar_chart(df["LA", "Math"])
-# chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.title("Math")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-# st.bar_chart(chart_data)
-# #Line Graph
-# st.title("Physics")
-# st.line_chart(df["Physics"])
+st.bar_chart(chart_data)
+#Line Graph
+st.title("Physics")
+st.line_chart(df["Physics"])
 
-# #Scatter plot
-# df["Total"]=df["LA"]+df["Math"]+df["Physics"]+df["SS"]+df["PE"]
-# st.title("Math & Physics")
-# st.scatter_chart(df, x = "Physics", y = "Math", size="Total")
+#Scatter plot
+df["Total"]=df["LA"]+df["Math"]+df["Physics"]+df["SS"]+df["PE"]
+st.title("Math & Physics")
+st.scatter_chart(df, x = "Physics", y = "Math", size="Total")
 
 #Map with Scatter plot
-# st.title("San Francisco")
+st.title("San Francisco")
 df = pd.DataFrame(
     np.random.randn(200, 2) / [50, 50] + [37.76, -122.45],
     columns=['lat', 'lon'])
--- st_input.py
+++ st_input.py
@@ -5,48 +5,48 @@ import streamlit as st
 string = st.text_input("Enter a message")
 st.write(string)
 
-# num1 = st.number_input("Enter an integer", min_value = 0, max_value=100)
-# st.write(num1)
+num1 = st.number_input("Enter an integer", min_value = 0, max_value=100)
+st.write(num1)
 
-# num2 = st.slider("Today's feeling", min_value = 0, max_value=100, step = 10)
-# st.write(num2)
+num2 = st.slider("Today's feeling", min_value = 0, max_value=100, step = 10)
+st.write(num2)
 
-# st.button("Reset", type="primary")
-# if st.button("Say hello"):
-#     st.write("Hello Friend!")
-# else:
-#     st.write("Goodbye")
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Hello Friend!")
+else:
+    st.write("Goodbye")
 
-# is_agree = st.checkbox("Can you commit doing homework?")
-# if is_agree:
-#     st.write("Great!")
-# else:
-#     st.write("You better do it")
+is_agree = st.checkbox("Can you commit doing homework?")
+if is_agree:
+    st.write("Great!")
+else:
+    st.write("You better do it")
 
-# options = st.multiselect(
-#     "What are your favorite colors (Multiple selection)?",
-#     ["Green", "Yellow", "Red", "Blue","Purple"],
-#     ["Yellow", "Red"])
-# st.write("You selected:", options)
+options = st.multiselect(
+    "What are your favorite colors (Multiple selection)?",
+    ["Green", "Yellow", "Red", "Blue","Purple"],
+    ["Yellow", "Red"])
+st.write("You selected:", options)
 
-# on = st.toggle("Activate feature", value=True)
-# if on:
-#     st.write("Feature activated!")
-# else:
-#     "Feature deactivated"
+on = st.toggle("Activate feature", value=True)
+if on:
+    st.write("Feature activated!")
+else:
+    "Feature deactivated"
 
-# import datetime
-# today = datetime.datetime.now()
-# next_year = today.year + 1
-# jan_1 = datetime.date(next_year, 1, 1)
-# dec_31 = datetime.date(next_year, 12, 31)
+import datetime
+today = datetime.datetime.now()
+next_year = today.year + 1
+jan_1 = datetime.date(next_year, 1, 1)
+dec_31 = datetime.date(next_year, 12, 31)
 
-# period = st.date_input(
-#     "Select your vacation for next year",
-#     (jan_1, datetime.date(next_year, 1, 7)),
-#     jan_1,
-#     dec_31,
-#     format="MM.DD.YYYY",
-# )
-# period
+period = st.date_input(
+    "Select your vacation for next year",
+    (jan_1, datetime.date(next_year, 1, 7)),
+    jan_1,
+    dec_31,
+    format="MM.DD.YYYY",
+)
+period
 
